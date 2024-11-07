21d388b1983a0000caad6028b13e5a82eeed487c
madhu kethireddy <madhukethireddy.k@gmail.com>
2024-11-07 10:31:42 +0530

    refactor: remove = in main method for loop

--- Fibonacci.java
+++ Fibonacci.java
@@ -19,7 +19,7 @@ class Fibonacci{
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i=0; i<=n; i++){
+		for(int i=0; i<n; i++){
 			System.out.print(fib(i)+" ");
 		}
 	}
