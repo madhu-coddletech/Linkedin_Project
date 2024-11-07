081f04b176360823a11fff2f4dca8c7de2e66054
madhu kethireddy <madhukethireddy.k@gmail.com>
2024-11-07 10:24:48 +0530

    feat: added fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Fibonacci{
+
+	public static int fib(int n){
+		if(n==0) return 0;
+		if(n==1) return 1;
+		int firstNum = 0;
+		int secondNum = 1;
+		for(int i=2; i<=n; i++){
+			int total = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = total;
+		}
+		return secondNum;
+
+	}
+	public static void main(String... args){
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0; i<=n; i++){
+			System.out.print(fib(i)+" ");
+		}
+	}
+}
